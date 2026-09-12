c6e2e180247c5961b0dea88835ccdef9ab711f25
AmeZer <34923959+Bissiking@users.noreply.github.com>
2026-09-12 22:27:45 +0200

    fix(server): allow external bind via HOSTNAME

--- server.ts
+++ server.ts
@@ -1,10 +1,11 @@
+// server.ts
 import { createServer } from "http";
 import { parse } from "url";
 import next from "next";
 import { WebSocketServer, WebSocket } from "ws";
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+const hostname = process.env.HOSTNAME || "0.0.0.0";
 const port = parseInt(process.env.PORT || "3002", 10);
 
 const app = next({ dev, hostname, port });
